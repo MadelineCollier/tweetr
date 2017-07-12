d6751086bf3d8d945f1ddd864fc005fa51a71644
Madeline Collier <madel@telus.net>
2017-07-12 02:10:56 +0000

    refactored createTweetElement so the structure of what was being appended was more clear, implemented renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-
 $(document).ready(function() {
 
-  var tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+  //These three functions create the header, body and footer of the tweet
+  //they were taken out of the createTweetElement for readability,
+  //and to create more intuitive level of abstraction in the createTweetElement function
+  function createTweetHeader (tweetData) {
+    var $header = $("<header>")
+      .append($("<img class='avatar' src='" + tweetData.user.avatars.small + "'>"))
+      .append($("<h1>").text(tweetData.user.name))
+      .append($("<h2>").text(tweetData.user.handle))
+    return $header;
   }
 
-  function createTweetElement (tweetObj) {
-    var $tweet = $("<article>").addClass("tweet")
-      .append($("<header>")
-        .append($("<img class='avatar' src='" + tweetData.user.avatars.small + "'>"))
-        .append($("<h1>").text(tweetData.user.name))
-        .append($("<h2>").text(tweetData.user.handle))
-      )
-      .append($("<p>").text(tweetData.content.text))
-      .append($("<footer>")
-        .append($("<h3>").text(tweetData.created_at))
-        .append($("<span>").addClass("icons")
-          .append($("<img class='flag' src='/images/flag.png'>"))
-          .append($("<img class='retweet' src='/images/retweet.png'>"))
-          .append($("<img class='like' src='/images/like.png'>"))
-        )
-      )
+  function createTweetBody (tweetData) {
+    var $body = $("<p>").text(tweetData.content.text)
+    return $body;
+  }
+
+  function createTweetFooter (tweetData) {
+    var $footer = $("<footer>")
+      .append($("<h3>").text(tweetData.created_at))
+      .append($("<span class='icons'>")
+        .append($("<img class='flag' src='/images/flag.png'>"))
+        .append($("<img class='retweet' src='/images/retweet.png'>"))
+        .append($("<img class='like' src='/images/like.png'>")))
+    return $footer;
+  }
+
+
+
+
+
+
+  //takes in a single tweetData object
+  //calls functions for header, body, footer, and appends them to the article tweet
+  //reduced to function calls to make the structure/content of the appends more intelligible
+  function createTweetElement (tweetData) {
+    var $tweet = $("<article class='tweet'>")
+      .append(createTweetHeader(tweetData))
+      .append(createTweetBody(tweetData))
+      .append(createTweetFooter(tweetData));
+
     return $tweet;
-  }
-
-  var $tweet = createTweetElement(tweetData);
-
-
-  $('#tweets-container').append($tweet);
+  };
 
 
 
+
+  //takes in "tweets" (an array of objects each containing tweet info)
+  //iterates through "tweets" calling createTweetElement for each given object in the array
+  //appends each of the final results of createTweetElement to the DOM
+  function renderTweets (tweets) {
+    tweets.forEach(function(tweetObj) {
+      var tweet = createTweetElement(tweetObj);
+      $("#tweets-container").append(tweet);
+    })
+  };
 });
 
 
